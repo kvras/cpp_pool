2930a29f2734be7a094c4123f478f25bfef0cdba
kvras <iguiji.etudes@gmail.com>
2024-11-28 23:53:28 +0100

    Merging the 2 containers in one template algo

--- cpp_09/ex02/PmergeMe.hpp
+++ cpp_09/ex02/PmergeMe.hpp
@@ -25,3 +25,38 @@ void insertion_sort(T& container)
             
     }   
 }
+
+template <typename V>
+
+V merge_insertion_sort(V& container, int argc, char **argv)
+{
+    V second_container;
+    V first_container;
+
+    for (int i = 1;i  < argc;i++)
+        container.push_back(std::atoi(argv[i]));   
+    for (size_t i = 0; i < container.size() ; i+=2)
+    {
+       if (container[i]  < container[i + 1])
+        {    
+            int tmp = container[i];
+            container[i] =  container[i + 1];
+            container[i + 1] = tmp;
+        }
+    }
+    for(size_t i = 0; i < container.size() - 1; i+=2)
+    {
+        first_container.push_back(container[i]);
+        second_container.push_back(container[i + 1]);
+    }
+
+    if (container.size() % 2 == 1)
+        second_container.push_back(container[container.size() - 1]);
+    insertion_sort(container);
+    for (auto it = second_container.begin(); it != second_container.end(); it++)
+    {
+        auto iterat = std::lower_bound(first_container.begin(), first_container.end(), *it);
+        first_container.insert(iterat, *it);
+    }
+    return first_container;
+}
--- cpp_09/ex02/main.cpp
+++ cpp_09/ex02/main.cpp
@@ -10,82 +10,25 @@ int main(int argc, char *argv[])
         std::cout << "Error" << std::endl;
         return 1;
     }
+
     std::clock_t start = std::clock();
     std::vector<int> vect;
-    for (int i = 1;i  < argc;i++)
-        vect.push_back(std::atoi(argv[i]));   
-    for (size_t i = 0; i < vect.size() ; i+=2)
-    {
-       if (vect[i]  < vect[i + 1])
-        {    
-            int tmp = vect[i];
-            vect[i] =  vect[i + 1];
-            vect[i + 1] = tmp;
-        }
-    }
-    std::vector<int> first_vect;
-    std::vector<int> second_vect;
-    for(size_t i = 0; i < vect.size() - 1; i+=2)
-    {
-        first_vect.push_back(vect[i]);
-        second_vect.push_back(vect[i + 1]);
-    }
-
-    if (vect.size() % 2 == 1)
-        second_vect.push_back(vect[vect.size() - 1]);
-    insertion_sort(vect);
-    for (auto it = second_vect.begin(); it != second_vect.end(); it++)
-    {
-        auto iterat = std::lower_bound(first_vect.begin(), first_vect.end(), *it);
-        first_vect.insert(iterat, *it);
-    }
-
+    std::vector<int> ret_vect = merge_insertion_sort(vect, argc, argv);
     std::clock_t end = std::clock();
+
     double elapsed_time = double(end - start) / CLOCKS_PER_SEC;
     std::cout << "Elapsed time: " << elapsed_time << " seconds\n";
     std::cout << "vect : ";
-    
-    for(auto it = first_vect.begin();it!= first_vect.end();it++)
+    for(auto it = ret_vect.begin();it!= ret_vect.end();it++)
         std::cout << *it << " ";
-
-    // std::cout << "\nsecond : ";
-    // for(auto it = second_vect.begin();it!= second_vect.end();it++)
-    //     std::cout << *it << " ";
 
     std::clock_t start1 = std::clock();
     std::deque<int> deque;
-    for (int i = 1;i  < argc;i++)
-        deque.push_back(std::atoi(argv[i]));   
-    for (size_t i = 0; i < deque.size() ; i+=2)
-    {
-       if (deque[i]  < deque[i + 1])
-        {    
-            int tmp = deque[i];
-            deque[i] =  deque[i + 1];
-            deque[i + 1] = tmp;
-        }
-    }
-    std::deque<int> first_deque;
-    std::deque<int> second_deque;
-    for(size_t i = 0; i < deque.size() - 1; i+=2)
-    {
-        first_deque.push_back(deque[i]);
-        second_deque.push_back(deque[i + 1]);
-    }
-
-    if (deque.size() % 2 == 1)
-        second_deque.push_back(deque[deque.size() - 1]);
-    insertion_sort(deque);
-    for (auto it = second_deque.begin(); it != second_deque.end(); it++)
-    {
-        auto iterat = std::lower_bound(first_deque.begin(), first_deque.end(), *it);
-        first_deque.insert(iterat, *it);
-    }
-
+    std::deque<int> ret_deque = merge_insertion_sort(deque, argc, argv);
     std::clock_t end1 = std::clock();
     double elapsed_time1 = double(end1 - start1) / CLOCKS_PER_SEC;
     std::cout << "\nElapsed time: " << elapsed_time1 << " seconds\n";
     std::cout << "deque : ";
-    for(auto it = first_deque.begin();it!= first_deque.end();it++)
+    for(auto it = deque.begin();it!= deque.end();it++)
         std::cout << *it << " ";
 }
